9c8f7babc0ed6973efe8ad60c0459e39b2cfb8f0
jimboo7339 <jimboo7339@users.noreply.github.com>
2026-06-10 21:37:00 +0800

    fix: 去掉roundIcon引用避免资源找不到

--- .github/scripts/apply_android_config.py
+++ .github/scripts/apply_android_config.py
@@ -10,7 +10,6 @@ MANIFEST = '''<manifest xmlns:android="http://schemas.android.com/apk/res/androi
         android:label="飞牛TV"
         android:name="${applicationName}"
         android:icon="@mipmap/ic_launcher"
-        android:roundIcon="@mipmap/ic_launcher_round"
         android:usesCleartextTraffic="true"
         android:networkSecurityConfig="@xml/network_security_config"
         android:allowBackup="true">
